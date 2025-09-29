aee0d4b436669a3ef631ac8a0ff9ee1e44b87ab6
Tito Müller <103798266+TitoMuller@users.noreply.github.com>
2025-09-29 16:38:13 -0300

    Delete microservice directory

--- microservice/dist/routes/dora-routes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as doraRoutes };
-//# sourceMappingURL=dora-routes.d.ts.map
--- microservice/dist/utils/query-builder.d.ts
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=query-builder.d.ts.map
